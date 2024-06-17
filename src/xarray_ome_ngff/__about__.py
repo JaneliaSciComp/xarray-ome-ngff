b0ea15c924ea64e8a08128029820b9a29001ebd1
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-06-17 21:26:28 +0200

    remove some default stuff added by the hatch init

--- src/xarray_ome_ngff/__about__.py
+++ src/xarray_ome_ngff/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
+# SPDX-FileCopyrightText: 2024-present Howard Hughes Medical Institute
 #
 # SPDX-License-Identifier: MIT
 __version__ = "2.2.0"
